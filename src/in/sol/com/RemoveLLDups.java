96d439d0cebeb92b54055f603326791d01f17263
Athreya KS <ksathreya94@nsoft.in>
2019-06-21 17:11:13 +0530

    Second Commit

---
+++ src/in/sol/com/RemoveLLDups.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package in.sol.com;
+
+import java.util.Scanner;
+
+/**
+ * @author athreya.ks
+ *
+ */
+class Node1 {
+	int data;
+	Node1 next;
+
+	Node1(int d) {
+		data = d;
+		next = null;
+	}
+}
+
+public class RemoveLLDups {
+
+	public static Node1 removeDuplicates(Node1 head) {
+		if (head == null) {
+			return null;
+		}
+		Node1 n = head;
+		while (n.next != null) {
+			if (n.data == n.next.data) {
+				n.next = n.next.next;
+			} else {
+				n = n.next;
+			}
+		}
+		return head;
+	}
+
+	public static Node1 insert(Node1 head, int data) {
+		Node1 p = new Node1(data);
+		if (head == null)
+			head = p;
+		else if (head.next == null)
+			head.next = p;
+		else {
+			Node1 start = head;
+			while (start.next != null)
+				start = start.next;
+			start.next = p;
+		}
+		return head;
+	}
+
+	public static void display(Node1 head) {
+		Node1 start = head;
+		while (start != null) {
+			System.out.print(start.data + " ");
+			start = start.next;
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Node1 head = null;
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int ele = sc.nextInt();
+			head = insert(head, ele);
+		}
+		head = removeDuplicates(head);
+		display(head);
+	}
+
+}
