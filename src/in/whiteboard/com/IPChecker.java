110de12e6e4f5b49653165a76995653db8781aef
Athreya KS <ksathreya94@nsoft.in>
2019-07-18 13:02:02 +0530

    Added Regex problems

---
+++ src/in/whiteboard/com/IPChecker.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package in.whiteboard.com;
+
+import java.util.Scanner;
+
+/**
+ * @author athreya.ks
+ *
+ */
+public class IPChecker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+
+	}
+
+}
+/*
+*	[01]?\\d{1,2}   matches numbers 0-199.
+*	2[0-4]\\d       matches numbers 200-249
+*	25[0-5]         matches numbers 250-255
+*/
+class MyRegex {
+	String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+	String pattern = num + "." + num + "." + num + "." + num;
+}
