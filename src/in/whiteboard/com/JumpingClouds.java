b7f83824ff1dfd0c7021cd46ca44fa9cc280aeb9
Athreya KS <ksathreya94@nsoft.in>
2019-07-19 18:39:27 +0530

    One more added

---
+++ src/in/whiteboard/com/JumpingClouds.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package in.whiteboard.com;
+
+import java.util.Scanner;
+
+/**
+ * @author athreya.ks
+ *
+ */
+public class JumpingClouds {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/* Save input */
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] cloud = new int[n];
+		for (int i = 0; i < n; i++) {
+			cloud[i] = scan.nextInt();
+		}
+		scan.close();
+
+		/* Jump on clouds */
+		int jumps = 0;
+		int i = 0;
+		while (i < n - 1) {
+			if (i + 2 < n && cloud[i + 2] == 0) {
+				i += 2;
+			} else {
+				i++;
+			}
+			jumps++;
+		}
+		System.out.println(jumps);
+	}
+
+}
