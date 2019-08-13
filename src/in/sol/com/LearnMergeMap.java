96d79ab588374458cbb72c6582416fe473447671
Athreya KS <ksathreya94@nsoft.in>
2019-08-13 15:34:48 +0530

    Abstract concepts

---
+++ src/in/sol/com/LearnMergeMap.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package in.sol.com;
+
+import java.util.function.BiFunction;
+
+/**
+ * @author athreya.ks
+ *
+ */
+public class LearnMergeMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
